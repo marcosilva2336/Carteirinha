2a3fd81f6c4f7bbccf462360531f2133476a9d7f
João Marcos <114954938+marcosilva2336@users.noreply.github.com>
2024-07-16 01:32:25 -0300

    Update api.js

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://carteirinha-back-psi.vercel.app/api';
 
 export const createUser = async (name, cpf) => {
   try {
